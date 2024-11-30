225c29ef785d5c2ae1be603db4b20f5c805be573
Jacob Vardilos <jvardilos@detritus.attlocal.net>
2024-11-30 13:58:14 -0800

    translate the text, only can do 2000 rows for now

--- translation.r
+++ translation.r
@@ -5,16 +5,12 @@ library(stringr)
 library(dplyr)
 
 translation <- read_csv("translation.csv")
-poems <- read_csv("poems.csv")
+all_poems <- read_csv("poems.csv")
+poems <- all_poems[1:2000, ]
 
 make_translation_map <- function(translation) {
   translation$en <- paste0("[", translation$en, "]")
   return(setNames(translation$en, translation$ru))
-}
-
-fun <- function(tokens, tm) {
-  tokens[tokens %in% names(tm)] <- tm[tokens[tokens %in% names(tm)]]
-  return(tokens)
 }
 
 translate <- function(text, translation) {
@@ -23,10 +19,11 @@ translate <- function(text, translation) {
   lapply(tolower(text), function(text) {
     tokens <- unlist(str_split(text, "\\s+"))
     tokens <- tokens[grepl("^\\b\\w+\\b$", tokens)]
-    return(fun(tokens, tm))
+    tokens[tokens %in% names(tm)] <- tm[tokens[tokens %in% names(tm)]]
+    combined <- paste(tokens, collapse = " ")
+    return(combined)
   })
 }
-
 
 poems$translation <- translate(poems$text, translation)
 poems$translation[1:23]
