8d8a92402885de4a5bb7d92167b1707c9dabc41f
Jacob Vardilos <jvardilos@detritus.attlocal.net>
2024-11-30 16:08:37 -0800

    get average words in a line

--- translation.r
+++ translation.r
@@ -6,24 +6,53 @@ library(dplyr)
 
 translation <- read_csv("translation.csv")
 all_poems <- read_csv("poems.csv")
-poems <- all_poems[1:2000, ]
+poems <- all_poems[1:200, ]
 
-make_translation_map <- function(translation) {
-  translation$en <- paste0("[", translation$en, "]")
-  return(setNames(translation$en, translation$ru))
+get_tokens <- function(text) {
+  tokens <- unlist(str_split(text, "\\s+"))
+  return(tokens[grepl("^\\b\\w+\\b$", tokens)])
 }
 
 translate <- function(text, translation) {
-  tm <- make_translation_map(translation)
+  translation$en <- paste0("[", translation$en, "]")
+  tm <- setNames(translation$en, translation$ru)
 
-  lapply(tolower(text), function(text) {
-    tokens <- unlist(str_split(text, "\\s+"))
-    tokens <- tokens[grepl("^\\b\\w+\\b$", tokens)]
-    tokens[tokens %in% names(tm)] <- tm[tokens[tokens %in% names(tm)]]
-    combined <- paste(tokens, collapse = " ")
-    return(combined)
+  result <- lapply(tolower(text), function(text) {
+    tokens <- get_tokens(text)
+    translated_tokens <- tm[tokens[tokens %in% names(tm)]]
+    rate <- length(translated_tokens) / length(tokens)
+
+    tokens[tokens %in% names(tm)] <- translated_tokens
+    translated <- paste(tokens, collapse = " ")
+    return(list(translated = translated, rate = rate))
   })
+
+  result_df <- do.call(rbind, lapply(result, as.data.frame))
+  return(result_df)
 }
 
-poems$translation <- translate(poems$text, translation)
-poems$translation[1:23]
+average_words_per_line <- function(text) {
+  words <- 0
+  lines <- str_split(text, "\n")
+  total_lines <- length(lines[[1]])
+
+  for (line in lines) {
+    tokens <- get_tokens(line)
+    words <- words + length(tokens)
+  }
+  average <- words / total_lines
+
+  return(average)
+}
+
+
+translation <- translate(poems$text, translation)
+poems$translation <- translation$translated
+poems$translation_rate <- as.numeric(translation$rate)
+
+poems$lines_rate <- sapply(poems$text, average_words_per_line)
+
+hist(scale(poems$translation_rate))
+
+out <- poems[, c("translation", "translation_rate", "lines_rate")]
+write.csv(out, file = "translation_effort.csv", row.names = FALSE)
