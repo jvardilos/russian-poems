72a360f749eb046e3c6ba5fb054c51d7b2dd9a98
Jacob Vardilos <jvardilos@detritus.attlocal.net>
2024-12-06 16:07:51 -0800

    some printlns haha, group by authors and grab my variables I want from the dataset

--- translation.r
+++ translation.r
@@ -6,7 +6,9 @@ library(dplyr)
 
 translation <- read_csv("translation.csv")
 all_poems <- read_csv("poems.csv")
-poems <- all_poems[1:200, ]
+poems <- all_poems[1:3000, ]
+
+str(all_poems)
 
 get_tokens <- function(text) {
   tokens <- unlist(str_split(text, "\\s+"))
@@ -24,10 +26,12 @@ translate <- function(text, translation) {
 
     tokens[tokens %in% names(tm)] <- translated_tokens
     translated <- paste(tokens, collapse = " ")
+    # print(as.list(tokens))
     return(list(translated = translated, rate = rate))
   })
 
   result_df <- do.call(rbind, lapply(result, as.data.frame))
+  # print(result_df)
   return(result_df)
 }
 
@@ -47,12 +51,44 @@ average_words_per_line <- function(text) {
 
 
 translation <- translate(poems$text, translation)
+
+translation$tokens
+translation$translated
 poems$translation <- translation$translated
 poems$translation_rate <- as.numeric(translation$rate)
-
+# poems$tokens <- translation$tokens
+# poems$translated_tokens <- translation$translated_tokens
 poems$lines_rate <- sapply(poems$text, average_words_per_line)
 
-hist(scale(poems$translation_rate))
 
-out <- poems[, c("translation", "translation_rate", "lines_rate")]
-write.csv(out, file = "translation_effort.csv", row.names = FALSE)
+### end cleaning
+
+aggregate_authors <- function(poems) {
+  ag <- poems %>%
+    group_by(writer) %>%
+    summarise(
+      translation_rate = mean(translation_rate),
+      lines_rate = mean(lines_rate),
+      # favorite_word = mode(tokens),
+      # favorite_translated_word = mode(translated_tokens),
+    )
+  # remove NaNs from the aggregation
+  ag$translation_rate[is.nan(ag$translation_rate)] <- 0
+  return(ag)
+}
+
+
+ag <- aggregate_authors(poems)
+
+
+highest_translation_rate <- ag[order(ag$translation_rate, decreasing = TRUE), ]
+highest_lines_rate <- ag[order(ag$lines_rate, decreasing = TRUE), ]
+lowest_lines_rate <- ag[order(ag$lines_rate, decreasing = FALSE), ]
+
+# plot(highest_lines_rate$translation_rate)
+
+### favorite word by author
+### authors most used translated word - mode
+### shortest Lines by author
+
+write.csv(highest_translation_rate, file = "translation_effort.csv", row.names = FALSE)
